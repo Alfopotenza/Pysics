ec9843e56044820f4e4384dcbef2b2ba71ba2843
Alfopotenza <alfotube1907dev@gmail.com>
2024-11-26 15:30:48 +0100

    Added friction Added other formulas

--- Objects.py
+++ Objects.py
@@ -46,25 +46,90 @@ class Vector:
         elif self.direction < 180:
             return self.direction == (other.direction + 180)
 
+    def __neg__(self):
+        if self.direction + 180 >= 360:
+            return Vector(self.module, self.direction - 180)
+        elif self.direction - 180 < 0:
+            return Vector(self.module, self.direction + 180)
+        else:
+            return Vector(self.module, self.direction + 180)
+
+    def get_comps(self, env, **comps):
+        steepness = env.steepness
+        if comps.get("x") is None and comps.get("y") is None:
+            comp_x = Vector(self.module * sin(steepness), self.direction + 90 - steepness)
+            comp_y = Vector(self.module * cos(steepness), self.direction - steepness)
+            return {"x": comp_x, "y": comp_y}
+        else:
+            mod = comps.get("x").module / sin(steepness)
+            _dir = 270
+            return Vector(mod, _dir)
+
+
 class Object:
+    _vars = {}
+    forces = []
+
     def __init__(self, environment, **_vars):
         #here all the variables that can exist will be added
+        self.hooke_force = None
+        self.weight = None
         self.acceleration = None
-        self.mass = _vars.get("mass")
-        self.force = _vars.get("force")
+
+        self._vars = _vars
         self.env = environment
-    def show(self, _ax, origin, color, size):
-        return _ax.add_patch(plt.Circle(origin, size, color = color))
-    def newton_first_law(self):
+
+        self.mass = _vars.get("mass")
+
+    def show_obj(self, _ax, origin, color, size):
+        return _ax.add_patch(plt.Circle(origin, size, color=color))
+
+    def show_forces(self, _ax, origin, color):
+        for force in self.forces:
+            print(force)
+            force.show(_ax, origin, color)
+
+    def add_force(self, **variables):
+        force = Vector(module=variables.get("force"), direction=variables.get("direction"))
+        self.forces.append(force)
+
+    def hooke_law(self, **variables):
+        force = variables.get("force")
+        hooke_constant = variables.get("constant")
+        spring_length = variables.get("length")
+        spring_dir = variables.get("spring_dir")
+        if force is None:
+            return Vector(hooke_constant * spring_length, spring_dir)
+        if hooke_constant is None:
+            return force.module / spring_length
+        if spring_length is None:
+            return force.module / hooke_constant
+
+    def newton_first_law(self, **variables):
+        self.weight: Vector = variables.get("weight")
         self.acceleration = self.env.gravity
-        if self.force == 0:
+        if self.weight is None or self.weight.module == 0:
             return Vector(self.mass * self.acceleration, 270)
-        if self.mass == 0:
-            return self.force / self.acceleration
-        if self.acceleration == 0:
-            return self.force / self.mass
+        if self.mass is None:
+            return self.weight.module / self.acceleration
+        if self.acceleration is None:
+            return self.weight.module / self.mass
+
+    def friction_law(self, env, **variables):
+        steepness = env.steepness
+        force: Vector = variables.get("force")
+        pressing = variables.get("pressing")
+        fric_constant = variables.get("constant")
+        if force is None or force.module == 0:
+            return Vector(fric_constant * pressing.module, 180 - steepness)
+        elif pressing.module is None:
+            return force.module / fric_constant
+        elif fric_constant is None:
+            print("CHECK")
+            return force.module / pressing.module
+
+
 class Environment:
     def __init__(self, **_vars):
         self.gravity = _vars.get("gravity")
-
-
+        self.steepness = _vars.get("steepness")
--- main.py
+++ main.py
@@ -2,15 +2,35 @@ from Objects import *
 from matplotlib import pyplot as plt
 def main(**kwargs):
     fig, ax = plt.subplots()
+    '''Guide:
+    - Create an ENVIRONMENT. It takes in the constructor a kwargs where you have to put:
+    1. gravity
+    2. steepness of the plane the object is put on
+    ex: env = Environment(**{"gravity": 9.81, "steepness": 30})
+    - create you OBJECT. It takes in the constructor a kwargs where you have to put:
+    1. mass
+    2. environment
+    ex: corp1 = Object(**{"mass": 110, "environment": env})
+    - now you can use the functions inside of Object to create your forces. 
+    ex: weight = corp1.newton_first_law()
+    - if steepness is != 0, you can calculate the components of a force
+    ex: comps = weight.get_comps(env)
+    - by creating a plot with fig, ax = subplots() you can put it into a .show(ax, origin, color:str) function of the Vector to plot the vector
+    Inverse formulas have been added but require further testing.'''
+    env = Environment(**{"gravity": 9.81, "steepness": 30})
+    corp1 = Object(**{"mass": 110, "environment": env})
+    weight = corp1.newton_first_law()
+    comps1 = weight.get_comps(env)
+    print(comps1.get("x"))
 
-    env = Environment(**{"gravity": 9.81})
-    corp = Object(env, **{"mass": 8, "force": 0})
-    corp.show(ax, [0, 0], "k", 15)
-    result = corp.newton_first_law()
-    if type(result) == Vector:
-        result.show(ax, [0, 0], "r")
-    ax.set_xlim([-100, 100])
-    ax.set_ylim([-100, 100])
+    weight.show(ax, [0, 0], "r")
+    comps1.get("x").show(ax, [0, 0], "b")
+    comps1.get("y").show(ax, [0, 0], "b")
+
+    ax.set_xlim([-1500, 1500])
+    ax.set_ylim([-1500, 1500])
+
     plt.grid()
     plt.show()
+
 main()
